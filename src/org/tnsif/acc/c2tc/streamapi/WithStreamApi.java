082c9a243d87502c9f10706c47f8abd1228993ab
Gagan m n <shravanmn89@gmail.com>
2026-08-24 23:36:55 +0530

    first commit

---
+++ src/org/tnsif/acc/c2tc/streamapi/WithStreamApi.java
@@ -0,0 +1,15 @@
+package org.tnsif.acc.c2tc.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class WithStreamApi {
+
+	public static void main(String[] args) {
+		List<Integer> numbers=Arrays.asList
+
+	}
+
+}
